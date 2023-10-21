e103e1fea6a3bdb35c47beab8d0ba40847a9ad08
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-10-21 16:13:23 -0700

    fix: colors

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -11,10 +11,10 @@ const Footer = () => (
 
     <div className="app__footer-links">
       <div className="app__footer-links_contact">
-        <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
-        <p className="p__opensans">+1 212-344-1230</p>
-        <p className="p__opensans">+1 212-555-1230</p>
+        <h1 className="app__footer-headtext" style={{color: "#F5EFDB"}}>Contact Us</h1>
+        <p className="p__opensans" style={{ color: "#AAA" }}>9 W 53rd St, New York, NY 10019, USA</p>
+        <p className="p__opensans"style={{ color: "#AAA" }}>+1 212-344-1230</p>
+        <p className="p__opensans"style={{ color: "#AAA" }}>+1 212-555-1230</p>
       </div>
 
       <div className="app__footer-links_logo">
@@ -34,16 +34,16 @@ const Footer = () => (
       </div>
 
       <div className="app__footer-links_work">
-        <h1 className="app__footer-headtext">Working Hours</h1>
-        <p className="p__opensans">Monday-Friday:</p>
-        <p className="p__opensans">08:00 am -12:00 am</p>
-        <p className="p__opensans">Saturday-Sunday:</p>
-        <p className="p__opensans">07:00am -11:00 pm</p>
+        <h1 className="app__footer-headtext" style={{color: "#F5EFDB"}}>Working Hours</h1>
+        <p className="p__opensans" style={{ color: "#AAA" }}>Monday-Friday:</p>
+        <p className="p__opensans" style={{ color: "#AAA" }}>08:00 am -12:00 am</p>
+        <p className="p__opensans" style={{ color: "#AAA" }}>Saturday-Sunday:</p>
+        <p className="p__opensans" style={{ color: "#AAA" }}>07:00am -11:00 pm</p>
       </div>
     </div>
 
     <div className="footer__copyright">
-      <p className="p__opensans">2021 Gerícht. All Rights reserved.</p>
+      <p className="p__opensans" style={{color: "#AAA"}}>2021 Gerícht. All Rights reserved.</p>
     </div>
   </div>
 );
